7eaa63243470ef959ef7978ec177823d1023a2b6
khairul111010 <khairul18103199@gmail.com>
2022-11-08 01:01:22 +0600

    education section done

--- pages/education.js
+++ pages/education.js
@@ -1,17 +1,52 @@
 import React from "react";
 import styles from "../styles/Education.module.css";
+import Image from "next/image";
+import dai from "../public/assets/img/dai.jpg";
 export default function education() {
   return (
     <div className={styles.education_wrapper}>
       <div className={styles.education_start}>
-        <div className={styles.education_header}>
-          <h2>Education</h2>
-          <div className={styles.education_start_cards}>
-            <div className={styles.education_start_card}>
-                
-            </div>
+        <div className={styles.education_header}>Education</div>
+      </div>
+      <div className={styles.cards}>
+        <div className={styles.card}>
+          <Image src={dai} />
+          <div className={styles.card_text}>
+            <p>MS in Computer Science</p>
+            <h3>Janurary 2021 - Present</h3>
+            <p>Relevant Coursework</p>
+            <ul>
+              <li>Distributed Database Systems</li>
+              <li>Cloud Computing</li>
+              <li>Foundation Of Algorithms</li>
+            </ul>
           </div>
         </div>
+        <div className={styles.card}>
+          <Image src={dai} />
+          <div className={styles.card_text}>
+            <p>MS in Computer Science</p>
+            <h3>Janurary 2021 - Present</h3>
+            <p>Relevant Coursework</p>
+            <ul>
+              <li>Distributed Database Systems</li>
+              <li>Cloud Computing</li>
+              <li>Foundation Of Algorithms</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.certificate_start}>
+        <div className={styles.certificate_header}>Certificate</div>
+      </div>
+      <div className={styles.cert_img}>
+        <Image src={dai} />
+        <Image src={dai} />
+        <Image src={dai} />
+        <Image src={dai} />
+        <Image src={dai} />
+        <Image src={dai} />
       </div>
     </div>
   );
